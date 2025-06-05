d845211396e3033f62817db7d636df6a20c3276e
Burry50531 <molodoy25666@gmail.com>
2025-06-05 21:17:19 +0300

    initial commit for hw2-mongodb

---
+++ src/db/initMongoConnection.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import { getEnvVar } from '../utils/getEnvVar.js';
+
+export const initMongoConnection = async () => {
+  const user = getEnvVar('MONGODB_USER');
+  const pwd = getEnvVar('MONGODB_PASSWORD');
+  const url = getEnvVar('MONGODB_URL');
+  const db = getEnvVar('MONGODB_DB');
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`,
+      { serverSelectionTimeoutMS: 30000 },
+    );
+    console.log('Mongo connection successfully established!');
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
