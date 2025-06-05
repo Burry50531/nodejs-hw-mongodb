19684e0fc52ed95b28c4e01e3d89c8b3e9da5e12
Burry50531 <molodoy25666@gmail.com>
2025-06-05 23:33:55 +0300

    js

---
+++ src/server.js
@@ -0,0 +1,36 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+
+import contactsRouter from './routes/contactsRouter.js';
+
+const PORT = process.env.PORT || 3000;
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use('/contacts', contactsRouter);
+
+  app.use(errorHandler);
+  app.use(notFoundHandler);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+  });
+};
