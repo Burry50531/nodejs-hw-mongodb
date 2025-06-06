8d4862c47638b321fa94902a6e750428a3df1d46
Burry50531 <molodoy25666@gmail.com>
2025-06-06 18:39:49 +0300

    jsss

--- src/routes/contactsRouter.js
+++ src/routes/contactsRouter.js
@@ -8,25 +8,28 @@ import {
 } from '../controllers/contactsControllers.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
-import { createContactSchema } from '../validation/contactsValidation.js';
-import { isValidId } from '../middlewares/isValidId.js';
-
 import { addContactSchema, updateContactSchema } from '../validation/contactsValidation.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = express.Router();
 
 router.get('/', ctrlWrapper(getContactsController));
+
 router.get('/:contactId', isValidId, ctrlWrapper(handleGetContactById));
+
 router.post(
   '/',
-  validateBody(createContactSchema),
+  validateBody(addContactSchema), 
   ctrlWrapper(createContactController),
 );
+
 router.patch(
   '/:contactId',
   isValidId,
   validateBody(updateContactSchema),
   ctrlWrapper(patchContactController),
 );
+
 router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
+
 export default router;
