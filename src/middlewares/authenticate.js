4e1948e0276c331f2903f635ed57045a134872c8
Burry50531 <molodoy25666@gmail.com>
2025-06-10 16:42:53 +0300

    js

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,6 +1,6 @@
 import createHttpError from 'http-errors';
-import { User } from '../db/models/user.js';
-import { Session } from '../db/models/session.js';
+import { User } from '../db/models/User.js';
+import { Session } from '../db/models/Session.js';
 
 export const authenticate = async (req, res, next) => {
   const authHeader = req.get('Authorization');
